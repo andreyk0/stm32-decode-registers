c6c9a2d2c1061b1de7241c363bdb9b66fc61d1ec
Andrey Kartashov <andrey.kartashov@gmail.com>
2019-06-21 22:33:15 -0400

    print tar entries

--- src/Run.hs
+++ src/Run.hs
@@ -2,8 +2,11 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Run (run) where
 
-import Import
+import           Import
+import           SVDData
 
 run :: RIO App ()
 run = do
   logInfo "We're inside the application!"
+  liftIO printTarEntries
+
--- src/SVDData.hs
+++ src/SVDData.hs
@@ -1,18 +1,31 @@
 {-# LANGUAGE TemplateHaskell #-}
 
 module SVDData
-  (
+  ( printTarEntries
   ) where
 
 
-import qualified Codec.Archive.Tar as Tar
-import           Data.ByteString   (ByteString)
-import qualified Data.ByteString   as BS
+import qualified Codec.Archive.Tar      as Tar
+import qualified Codec.Compression.GZip as GZ
+import qualified Data.ByteString        as BS
+import qualified Data.ByteString.Lazy   as LB
 import           Data.FileEmbed
 import           Import
 
 
 
+printTarEntries :: IO ()
+printTarEntries = printNextEntry stmTarEntries
+  where
+    printNextEntry Tar.Done        = pure ()
+    printNextEntry (Tar.Fail e)    = print e
+    printNextEntry (Tar.Next e es) = print (Tar.entryPath e) >> printNextEntry es
 
-stmTgz :: BS.ByteString
-stmTgz = $(embedFile "STMicro.tgz")
+
+
+stmTarEntries :: Tar.Entries Tar.FormatError
+stmTarEntries  = Tar.read $ GZ.decompress stmTgz
+
+
+stmTgz :: LB.ByteString
+stmTgz = LB.fromStrict $(embedFile "STMicro.tgz")
